e585559d9d484d36a9f5918fe010d614a64dc065
Adam McCurdy <adam@codetronix.com>
2018-05-13 22:15:52 -0700

    working random without limits

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Jiggler
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                var pt = Cursor.Position;
+
+                var rnd = new Random();
+                var movementX = rnd.Next(-4, 5);
+                var movementY = rnd.Next(-4, 5);
+
+                pt.Y += movementY;
+                pt.X += movementX;
+
+                Cursor.Position = pt;
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
